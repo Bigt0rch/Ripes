51512d3211a7515dcf70a513253f463a799e1ae7
Víctor Ramírez Castaño <95280897+Bigt0rch@users.noreply.github.com>
2025-06-02 15:55:02 +0200

    Correct comments trivial_exception_handler.asm

--- examples/assembly/interrupts/trivial_exception_handler.asm
+++ examples/assembly/interrupts/trivial_exception_handler.asm
@@ -2,7 +2,7 @@
 .text
 __start:
 la t0, handler # escribir direccion del hanlder en t0
-csrrw zero, mtvec, t0 # escribimos la direccion del handler en utvc
+csrrw zero, mtvec, t0 # escribimos la direccion del handler en mtvec
 csrrwi zero, mstatus, 1 # activamos las excepciones
 lw zero, 1 #acceso ilegal a memoria
 addi t1, x0, 1 
@@ -11,8 +11,8 @@ jal x0, exit
 handler:
 csrrw t0, mepc, zero # escribimos el pc que provocó la excepcion en t0
 addi t0, t0, 4 # Adelantamos el PC una instruccion
-csrrw zero, mepc, t0 # Excribimos el pc de nuevo en uepc
-mret # finalizamos el handler, escribiendo el uepc en el pc
+csrrw zero, mepc, t0 # Excribimos el pc de nuevo en mepc
+mret # finalizamos el handler, escribiendo el mepc en el pc
 
 exit:
 li a7, 93            # Código de la syscall para 'exit' (93 en RISC-V)
