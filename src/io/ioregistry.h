233f6ea7107cc1ca250c251a8bc02a7d6e76d773
Bigt0rch <vicdan0603@gmail.com>
2025-06-02 03:59:39 +0200

    Register new peripherals

--- src/io/ioregistry.h
+++ src/io/ioregistry.h
@@ -4,8 +4,11 @@
 #include <QWidget>
 
 #include "iodpad.h"
+#include "iokeyboard.h"
 #include "ioledmatrix.h"
+#include "ioplic.h"
 #include "ioswitches.h"
+#include "iotextout.h"
 
 /** @brief IORegistry
  *
@@ -18,7 +21,7 @@
 
 namespace Ripes {
 
-enum IOType { LED_MATRIX, SWITCHES, DPAD, NPERIPHERALS };
+enum IOType { LED_MATRIX, SWITCHES, DPAD, PLIC, KEYBOARD, TEXT_OUT, NPERIPHERALS };
 
 template <typename T>
 IOBase *createIO(QWidget *parent) {
@@ -31,11 +34,17 @@ using IOFactory = std::function<IOBase *(QWidget *parent)>;
 const static std::map<IOType, QString> IOTypeTitles = {
     {IOType::LED_MATRIX, "LED Matrix"},
     {IOType::SWITCHES, "Switches"},
-    {IOType::DPAD, "D-Pad"}};
+    {IOType::DPAD, "D-Pad"},
+    {IOType::PLIC, "PLIC"},
+    {IOType::KEYBOARD, "KEYBOARD"},
+    {IOType::TEXT_OUT, "Text out"}};
 const static std::map<IOType, IOFactory> IOFactories = {
     {IOType::LED_MATRIX, createIO<IOLedMatrix>},
     {IOType::SWITCHES, createIO<IOSwitches>},
-    {IOType::DPAD, createIO<IODPad>}};
+    {IOType::DPAD, createIO<IODPad>},
+    {IOType::PLIC, createIO<IOPLIC>},
+    {IOType::KEYBOARD, createIO<IOKeyboard>},
+    {IOType::TEXT_OUT, createIO<IOTextOut>}};
 
 } // namespace Ripes
 
